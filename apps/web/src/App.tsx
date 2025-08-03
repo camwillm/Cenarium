7348b1b923856057a735b476c5efd1104e720f41
Cameron Williams <camwillmofficial@gmail.com>
2025-08-02 23:08:10 -0400

    conversion jsx-tsx

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,29 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "@/components/Layouts";
-import { createPageUrl } from "@/utils/createPageUrl";
+import Pages from '@/pages'
+import { Toaster } from '@/components/ui/toaster'
 
-
-
-
-function WrappedRoute({ element, title }: { element: JSX.Element; title: string }) {
-  return <Layout currentPageName={title}>{element}</Layout>;
-}
-
-export default function App() {
+function App(): JSX.Element {
   return (
-    <Router>
-      <Routes>
-        {/* Landing page - no layout */}
-        <Route path="/" element={<Landing />} />
-
-        {/* Layout-wrapped routes */}
-        <Route path={createPageUrl("Home")} element={<WrappedRoute title="Home" element={<Home />} />} />
-        <Route path={createPageUrl("About")} element={<WrappedRoute title="About" element={<About />} />} />
-        <Route path={createPageUrl("Signup")} element={<WrappedRoute title="Signup" element={<Signup />} />} />
-        <Route path={createPageUrl("Profile")} element={<WrappedRoute title="Profile" element={<Profile />} />} />
-        <Route path={createPageUrl("Calculator")} element={<WrappedRoute title="Calculator" element={<MacroCalculator />} />} />
-        <Route path={createPageUrl("SuggestMeal")} element={<WrappedRoute title="SuggestMeal" element={<AIMealSuggester />} />} />
-      </Routes>
-    </Router>
-  );
+    <>
+      <Pages />
+      <Toaster />
+    </>
+  )
 }
+
+export default App
+
