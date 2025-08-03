a85612626b0ddbc70f4a0bf93a802a619fa9bc0f
Cameron Williams <camwillmofficial@gmail.com>
2025-08-02 22:27:13 -0400

    rework start - vite.config.ts

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,15 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: true
+  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@utils': path.resolve(__dirname, 'src/utils'),
-      '@entities': path.resolve(__dirname, 'src/entities'),
-      '@pages': path.resolve(__dirname, 'src/pages')
-    }
-  }
-})
+      '@': path.resolve(__dirname, './src'),
+    },
+    extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx', '.json']
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+}) 
